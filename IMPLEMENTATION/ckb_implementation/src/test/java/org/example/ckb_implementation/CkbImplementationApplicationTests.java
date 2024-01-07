793b143204c4524b5bda982d77b85e44c46f6972
alessandrosaccone <alessandrosaccone96@gmail.com>
2024-01-08 00:24:28 +0100

    Implementation started, project set up

---
+++ IMPLEMENTATION/ckb_implementation/src/test/java/org/example/ckb_implementation/CkbImplementationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ckb_implementation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CkbImplementationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
