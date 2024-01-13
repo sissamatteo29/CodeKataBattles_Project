8fe13503ccb9511c3f68e905bc46f0d5489a3d53
alessandrosaccone <alessandrosaccone96@gmail.com>
2024-01-13 16:39:15 +0100

    refreshing ales branch

---
+++ IMPLEMENTATION/ckb/src/main/java/org/example/ckb/CkbApplication.java
@@ -0,0 +1,13 @@
+package org.example.ckb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CkbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CkbApplication.class, args);
+	}
+
+}
