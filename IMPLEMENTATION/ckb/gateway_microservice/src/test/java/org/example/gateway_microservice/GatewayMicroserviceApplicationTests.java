2e247549277544e5b1aa24888500cf4ac5716efe
alessandrosaccone <alessandrosaccone96@gmail.com>
2024-01-13 16:33:58 +0100

    Github login added with login link

---
+++ IMPLEMENTATION/ckb/gateway_microservice/src/test/java/org/example/gateway_microservice/GatewayMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gateway_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
